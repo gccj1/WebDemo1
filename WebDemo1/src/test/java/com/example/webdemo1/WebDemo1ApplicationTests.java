9efdb7f61254a37dfc38906b4fd068c6f413c00d
gccj <2191618127@qq.com>
2024-05-24 11:35:19 +0800

    WebDemo

---
+++ WebDemo1/src/test/java/com/example/webdemo1/WebDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
