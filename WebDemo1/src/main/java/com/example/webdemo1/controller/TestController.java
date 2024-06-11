91ca829f5a9f978dd14749af75fb6b9cbb49abda
gccj <2191618127@qq.com>
2024-06-11 20:32:18 +0800

    Update dependencies and add new endpoint in TestController

--- WebDemo1/src/main/java/com/example/webdemo1/controller/TestController.java
+++ WebDemo1/src/main/java/com/example/webdemo1/controller/TestController.java
@@ -9,4 +9,9 @@ public class TestController {
     public String test(){
         return "hello world!";
     }
+
+    @GetMapping("/test")
+    public String test2(){
+        return "hello trial!";
+    }
 }
